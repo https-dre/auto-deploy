ca230208783cb6f85029d5450597c4257f0536eb
https-dre <diaso.andre@outlook.com>
2025-05-12 22:19:15 -0300

    update docker script

---
+++ up-docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+cd $1
+
+docker stop $2
+
+docker remove $2
+
+docker build -t $2:latest .
+
+docker image prune -f
+
+docker run -d --env-file .env.docker -p $3:$4 --name $2 $2
